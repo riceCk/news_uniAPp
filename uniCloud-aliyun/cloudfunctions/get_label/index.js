81b25535fded2feece3c795c4177c7069b4fcf95
riceCk <876496731@qq.com>
2021-01-14 18:46:26 +0800

    '2021/01/14'

--- uniCloud-aliyun/cloudfunctions/get_label/index.js
+++ uniCloud-aliyun/cloudfunctions/get_label/index.js
@@ -4,15 +4,24 @@ const db = uniCloud.database()
 const $ = db.command.aggregate
 exports.main = async (event, context) => {
 	// let label = await db.collection("label").get();
-	const {user_id} = event
+	const {user_id, type} = event
+	let matchObj = {}
+	if (type !== 'all') {
+		matchObj = {
+			current: true
+		}
+	}
 	let userinfo = await db.collection('user').doc(user_id).get()
 	userinfo = userinfo.data[0]
 	// 会有label_ids = ['leabel_id']
 	let label = await  db.collection('label')
 	.aggregate()
 	.addFields({
+		// 新增current参数，判断"label_ids"里面是否有$_id参数
 		current: $.in(['$_id', $.ifNull([userinfo.label_ids, []])])
 	})
+	// match 相当于sql中where，条件判断
+	.match(matchObj)
 	.end()
 	
 	//返回数据给客户端
